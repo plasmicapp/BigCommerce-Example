11b044025502c2c0c233413a080ef19928dee275
Samuel <samuel@plasmic.app>
2023-11-08 13:16:39 -0300

    Fix server-side fetching error

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -1,4 +1,6 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
+import { registerAll } from "@plasmicpkgs/commerce";
+import { registerCommerceProvider as registerBigCommerce } from "@/commerce/bigcommerce";
 
 export const PLASMIC = initPlasmicLoader({
   projects: [
@@ -12,7 +14,7 @@ export const PLASMIC = initPlasmicLoader({
   // For development, you can set preview to true, which will use the unpublished
   // project, allowing you to see your designs without publishing.  Please
   // only use this for development, as this is significantly slower.
-  preview: false,
+  preview: true,
 });
 
 // You can register any code components that you want to use here; see
@@ -23,3 +25,5 @@ export const PLASMIC = initPlasmicLoader({
 // https://docs.plasmic.app/learn/app-hosting/#set-a-plasmic-project-to-use-your-app-host
 
 // PLASMIC.registerComponent(...);
+registerAll(PLASMIC);
+registerBigCommerce(PLASMIC);
